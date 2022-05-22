c4330c21429b3b913459ac846043183e8b2d609d
persy <persytry@outlook.com>
2022-05-22 17:09:24 +0800

    tardev

--- os/linux/builder/tardev.sh
+++ os/linux/builder/tardev.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # 2022/05/21 10:29:10
 
-mkdir ssh
-cp -r ~/.ssh ssh
-cp ~/.git-credentials ssh
-echo '' > ssh/.ssh/known_hosts
-sed -i -e "s/63051/63050/" ssh/.ssh/config
-cd ssh
+mkdir mysshtmp && cd mysshtmp
+cp -r ~/.ssh ./
+cp ~/.git-credentials ./
+echo '' > .ssh/known_hosts
+sed -i -e "s/63051/63050/" .ssh/config
 rm ~/share/web/sys/ssh.tar.gz
 tar -czf ~/share/web/sys/ssh.tar.gz .
 cd ..
-rm -rf ssh
+rm -rf mysshtmp
 
 mkdir mydevtmp && cd mydevtmp
-ln -s ~/data/software/sys/linux/dev dev
+ln -s ~/data/software/sys/linux/dev ./
 ln -s ~/a/git/lang/py/setup/dev/os/linux/builder/devbuilder.sh ./
 rm ~/share/web/sys/dev.tar.gz
 tar -czhf ~/share/web/sys/dev.tar.gz .
