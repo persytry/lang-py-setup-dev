6b50edd3aa742f9f85cfeccbec3c61e9fb9880a8
persy <persytry@outlook.com>
2022-05-25 11:40:44 +0800

    "improve gbatch, add staged command"

--- t/py/git_batch.py
+++ t/py/git_batch.py
@@ -28,6 +28,14 @@ def hasDiff(path: str) -> bool:
         if not os.path.exists(path): return False
     return _hasDiff(path)
 
+def hasStaged(path: str) -> bool:
+    if path is None:
+        path = os.getcwd()
+    else:
+        path = os.path.expanduser(path)
+        path = os.path.realpath(path)
+        if not os.path.exists(path): return False
+    return _hasStaged(path)
 
 def pushable(path: str) -> bool:
     if path is None:
@@ -56,6 +64,10 @@ def walkRepoDiff(path: str):
         if _hasDiff(repo):
             yield repo
 
+def walkRepoStaged(path: str):
+    for repo in walkRepo(path):
+        if _hasStaged(repo):
+            yield repo
 
 def walkRepoPushable(path: str):
     for repo in walkRepo(path):
@@ -67,7 +79,7 @@ def commitAll(comment: str, path: str) -> None:
     if comment is None or len(comment) == 0:
         print('must input comment to commit')
         return
-    for repo in walkRepoDiff(path):
+    for repo in walkRepoStaged(path):
         print(repo)
         res = subprocess.run(['git', 'commit', '-m', f'"{comment}"'], check=False, timeout=60, text=True, shell=False, cwd=repo, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(res.stdout)
@@ -78,23 +90,27 @@ async def _asyncOne(cmd:str, repo:str):
     proc = await asyncio.create_subprocess_shell(cmd, cwd=repo)
     await asyncio.wait_for(proc.communicate(), timeout=60)
 
+def pullAllSync(path: str) -> None:
+    for repo in walkRepo(path):
+        print(repo)
+        res = subprocess.run(['git', 'pull'], check=False, timeout=60, text=True, shell=False, cwd=repo, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(res.stdout)
+        print(res.stderr)
+
 def pullAll(path: str) -> None:
-    # for repo in walkRepo(path):
-        # print(repo)
-        # res = subprocess.run(['git', 'pull'], check=False, timeout=60, text=True, shell=False, cwd=repo, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # print(res.stdout)
-        # print(res.stderr)
     fns = []
     for repo in walkRepo(path):
         fns.append(_asyncOne('git pull', repo))
     asyncio.run(asyncio.wait(fns))
 
+def pushAllSync(path: str) -> None:
+    for repo in walkRepoPushable(path):
+        print(repo)
+        res = subprocess.run(['git', 'push'], check=False, timeout=60, text=True, shell=False, cwd=repo, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(res.stdout)
+        print(res.stderr)
+
 def pushAll(path: str) -> None:
-    # for repo in walkRepoPushable(path):
-        # print(repo)
-        # res = subprocess.run(['git', 'push'], check=False, timeout=60, text=True, shell=False, cwd=repo, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # print(res.stdout)
-        # print(res.stderr)
     fns = []
     for repo in walkRepoPushable(path):
         fns.append(_asyncOne('git push', repo))
@@ -111,6 +127,9 @@ def printRepoDiff(path: str) -> None:
     for repo in walkRepoDiff(path):
         print(repo)
 
+def printRepoStaged(path: str) -> None:
+    for repo in walkRepoStaged(path):
+        print(repo)
 
 def printRepoPushable(path: str) -> None:
     for repo in walkRepoPushable(path):
@@ -127,6 +146,10 @@ def _hasDiff(path: str) -> bool:
     res = subprocess.run(['git', 'diff', '--quiet'], check=False, timeout=60, text=True, shell=False, cwd=path, stdout=subprocess.DEVNULL, universal_newlines=True, stderr=subprocess.DEVNULL)
     return res.returncode == 1
 
+
+def _hasStaged(path: str) -> bool:
+    res = subprocess.run(['git', 'diff', '--staged', '--quiet'], check=False, timeout=60, text=True, shell=False, cwd=path, stdout=subprocess.DEVNULL, universal_newlines=True, stderr=subprocess.DEVNULL)
+    return res.returncode == 1
 
 def _pushable(path: str) -> bool:
     res = subprocess.run(['git', 'cherry', '-v'], check=False, timeout=60, text=True, shell=False, cwd=path, stdout=subprocess.PIPE, universal_newlines=True, stderr=subprocess.DEVNULL)
@@ -146,16 +169,19 @@ def _walkRepo(path: str):
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--path', type=str, default=None)
+    parser.add_argument('--path', type=str, default=None, help='current directory as default')
     sub_parser = parser.add_subparsers(help='sub-commands')
-    sub_parser.add_parser('repo').set_defaults(func=lambda a: printRepo(a.path))
-    sub_parser.add_parser('diff').set_defaults(func=lambda a: printRepoDiff(a.path))
-    sub_parser.add_parser('pushable').set_defaults(func=lambda a: printRepoPushable(a.path))
-    sub = sub_parser.add_parser('commit')
-    sub.add_argument('-m', default=None)
-    sub.set_defaults(func=lambda a: commitAll(a.m, a.path))
-    sub_parser.add_parser('pull').set_defaults(func=lambda a: pullAll(a.path))
-    sub_parser.add_parser('push').set_defaults(func=lambda a: pushAll(a.path))
+    sub_parser.add_parser('repo', help='print all git repo').set_defaults(func=lambda a: printRepo(a.path))
+    sub_parser.add_parser('diff', help='print unstage repo').set_defaults(func=lambda a: printRepoDiff(a.path))
+    sub_parser.add_parser('staged', help='print unstage repo').set_defaults(func=lambda a: printRepoStaged(a.path))
+    sub = sub_parser.add_parser('commit', help='commit all repo which committable(staged repo)')
+    sub.add_argument('m', nargs='+')
+    sub.set_defaults(func=lambda a: commitAll(' '.join(a.m), a.path))
+    sub_parser.add_parser('pushable', help='print pushable repo').set_defaults(func=lambda a: printRepoPushable(a.path))
+    sub_parser.add_parser('pull', help='pull async').set_defaults(func=lambda a: pullAll(a.path))
+    sub_parser.add_parser('pulls', help='pull sync').set_defaults(func=lambda a: pullAllSync(a.path))
+    sub_parser.add_parser('push', help='push async').set_defaults(func=lambda a: pushAll(a.path))
+    sub_parser.add_parser('pushs', help='push sync').set_defaults(func=lambda a: pushAllSync(a.path))
     args = parser.parse_args()
     fn = getattr(args, 'func', None)
     if fn is not None:
