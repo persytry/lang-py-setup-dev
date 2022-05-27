d81c93fbd16123567d91489d921c9347286b1ff0
persy <persytry@outlook.com>
2022-05-27 21:58:39 +0800

    type wget

--- os/linux/builder/builddev.sh
+++ os/linux/builder/builddev.sh
@@ -4,7 +4,9 @@ export http_proxy=http://proxy.izds.top:63000
 
 mytmppath=/tmp/mytmp
 mkdir -p $mytmppath
-wget $myminiserve/sys/dev.tar.gz -O $mytmppath
+if type wget >/dev/null 2>&1; then
+    wget $myminiserve/sys/dev.tar.gz -O $mytmppath
+fi
 if [ -f "$mytmppath/dev.tar.gz" ]; then
     tar -xzf $mytmppath/dev.tar.gz -C $mytmppath
     #所需参数去参考dev.dockerfile
