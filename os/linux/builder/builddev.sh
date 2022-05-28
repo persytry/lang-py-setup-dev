7a8f84153ee023efa69e2c9ffc5419d02f66578f
persy <persytry@outlook.com>
2022-05-28 10:46:37 +0800

    up

--- os/linux/builder/builddev.sh
+++ os/linux/builder/builddev.sh
@@ -6,16 +6,21 @@ export https_proxy=$http_proxy
 export apt_source_switch=0
 #export myminiserve=
 #export myprivsvr=
+#export DESKTOP_SESSION=true
 
 mytmppath=/tmp/mytmp
 mkdir -p $mytmppath
 if type wget >/dev/null 2>&1; then
-    wget $myminiserve/sys/dev.tar.gz -O $mytmppath
+    wget $myminiserve/sys/dev.tar.gz
+    mv dev.tar.gz $mytmppath
+else
+    cp dev.tar.gz $mytmppath
 fi
+
 if [ -f "$mytmppath/dev.tar.gz" ]; then
     tar -xzf $mytmppath/dev.tar.gz -C $mytmppath
-    #所需参数去参考dev.dockerfile
     ./devbuilder.sh | tee devbuilder.log
 else
+    echo "error: cannot find the file: $mytmppath/dev.tar.gz"
     rm -rf $mytmppath
 fi
