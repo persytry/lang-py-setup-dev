c38300fc7b8940dfe972f07e961775ecc0da790d
persy <persytry@outlook.com>
2022-06-02 23:06:55 +0800

    up

--- os/linux/builder/devbuilder.sh
+++ os/linux/builder/devbuilder.sh
@@ -141,8 +141,8 @@ pip install pynvim
 
 #https://github.com/nodesource/distributions/blob/master/README.md#debinstall
 #wget https://deb.nodesource.com/setup_18.x
-#sudo ./setup_18.x
-#sudo apt-get update
+sudo ./setup_18.x
+sudo apt-get update
 #sudo apt-get install -y nodejs npm
 sudo apt-get install -y ./nodejs_18.2.0-deb-1nodesource1_amd64.deb ./npm_7.5.2+ds-2_all.deb
 sudo npm install -g yarn neovim bash-language-server wsl-open
